ff7ce1d9c7214465249a738651f4d820948dcf66
shihab544 <geb0105742@utn.edu.my>
2020-12-25 01:20:22 +0600

    Lots of changes done

--- app/src/main/java/com/example/shihab/activities/ProfileActivity.kt
+++ app/src/main/java/com/example/shihab/activities/ProfileActivity.kt
@@ -1,8 +1,11 @@
 package com.example.shihab.activities
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
 import com.example.shihab.R
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_profile.*
@@ -15,6 +18,9 @@ class ProfileActivity : AppCompatActivity() {
         setContentView(R.layout.activity_profile)
         firebaseAuth = FirebaseAuth.getInstance()
         txtEmail.text = firebaseAuth.currentUser?.email
+
+
+
 
         btnLogout.setOnClickListener {
             FirebaseAuth.getInstance().signOut()
